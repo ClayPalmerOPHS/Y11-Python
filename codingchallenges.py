3cb4636bd3efecd4d2b255d173f2068306b6fd3e
ClayPalmerOPHS <71497229+ClayPalmerOPHS@users.noreply.github.com>
2020-10-02 12:07:36 +0100

    Create codingchallenges.py

---
+++ codingchallenges.py
@@ -0,0 +1,8 @@
+#Welcome and register
+
+print("Welcome to the game! We hope you enjoy your time playing. Please register a new account")
+age=int(input("Please enter your age: "))
+gender=str(input("Please enter your gender: "))
+emailAdd=int(input("Please enter your email address: "))
+playerName=int(input("Please enter your player name: "))
+
