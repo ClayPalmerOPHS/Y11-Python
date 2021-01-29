d45d13b64ecaa342abd1ec76017d30476c7a4020
ClayPalmerOPHS <71497229+ClayPalmerOPHS@users.noreply.github.com>
2021-01-29 09:56:02 +0000

    Create selection005.py

---
+++ selection005.py
@@ -0,0 +1,14 @@
+raining = str(input("Is it raining? (Yes/No) ")).lower()
+
+if raining == 'yes':
+    windy = str(input("Is it windy? (Yes/No) ")).lower()
+    if windy == 'yes':
+        print("It is too windy for an umbrella")
+    elif windy == 'no':
+        print("Take an umbrella")
+    else:
+        print("Invalid input, restart questions.")
+elif raining == 'no':
+    print("Enjoy your day")
+else:
+    print("Invalid input, restart questions.")
