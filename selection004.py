0b23fbc5587339eb7dfcf8e85210dea1e0fc0ed0
ClayPalmerOPHS <71497229+ClayPalmerOPHS@users.noreply.github.com>
2021-01-29 09:44:30 +0000

    Create selection004.py

---
+++ selection004.py
@@ -0,0 +1,6 @@
+favColour = str(input("Enter your favourite colour: ")).lower()
+
+if favColour == 'red':
+    print("I like red too")
+else:
+    print("I dont like",favColour,",",favColour,"is for losers")
