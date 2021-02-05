23f162efd08d8099364a7435d4e7f5b09e6a30c4
ClayPalmerOPHS <71497229+ClayPalmerOPHS@users.noreply.github.com>
2021-02-05 12:40:59 +0000

    Create WHILE LOOP challenges07.py

---
+++ WHILE LOOP challenges07.py
@@ -0,0 +1,22 @@
+bottles = 10
+
+while bottles > 0:
+    print("")
+    print("There are",bottles,"green bottles hanging on the wall,")
+    print(bottles,"green bottles hanging on the wall,")
+    print("And if one green bottle should accidentally fall...")
+    print("")
+    answer = int(input("How many green bottles will be hanging on the wall? "))
+    bottles = bottles - 1
+    if answer == bottles:
+        print("")
+        print("There'll be",bottles,"bottles hanging on the wall!")
+    else:
+        while answer != bottles:
+            print("No, try again.")
+            print("")
+            answer = int(input("How many green bottles will be hanging on the wall? "))
+            print("")
+        print("There'll be",bottles,"bottles hanging on the wall!")
+        
+print("There'll be no green bottles hanging on the wall")
