b6a297dccb55e0e7bd6e2c225fb890f136b62193
ClayPalmerOPHS <71497229+ClayPalmerOPHS@users.noreply.github.com>
2021-02-05 11:13:25 +0000

    Create WHILE LOOP challenges01.py

---
+++ WHILE LOOP challenges01.py
@@ -0,0 +1,8 @@
+total = 0
+
+while total <= 50:
+    number = int(input("Enter a number: "))
+    total = total + number
+    print("Total=",total,)
+
+print("The total is now over 50 (Total=",total,")")
