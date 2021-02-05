377f32fe0bacf0c0cd348585b23873e08a267fe5
ClayPalmerOPHS <71497229+ClayPalmerOPHS@users.noreply.github.com>
2021-02-05 11:35:29 +0000

    Create WHILE LOOP challenges04.py

---
+++ WHILE LOOP challenges04.py
@@ -0,0 +1,13 @@
+loop = 'Y'
+count = 0
+
+while loop == 'Y':
+    name = str(input("Name someone you want to invite to your party: ")).lower()
+    print("")
+    print(name,"has been invited.")
+    print("")
+    count = count + 1
+    loop = str(input("Would you like to invite anyone else to the party? (Y/N): ")).upper()
+    print("")
+    
+print("You have invited",count,"people to the party.")
