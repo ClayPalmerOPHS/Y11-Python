d3dc7ead8b6e92236e92d7cdbeb78d880206df0f
ClayPalmerOPHS <71497229+ClayPalmerOPHS@users.noreply.github.com>
2021-02-05 11:27:40 +0000

    Create WHILE LOOP challenges03.py

---
+++ WHILE LOOP challenges03.py
@@ -0,0 +1,13 @@
+number= int(input("Enter a number: "))
+
+total= number
+
+repeatLoop= 'Y'
+
+while repeatLoop == 'Y':
+    number2= int(input("Enter another number: "))
+    total= number + number2
+    repeatLoop= str(input("Do you want to add another number (Y/N): ")).upper()
+
+print("")
+print("The total of the numbers you entered is",total,)    
