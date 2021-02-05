6c02a69fe6e79e863bd795a2844c16aa0fe4dea6
ClayPalmerOPHS <71497229+ClayPalmerOPHS@users.noreply.github.com>
2021-02-05 12:10:23 +0000

    Create WHILE LOOP challenges05.py

---
+++ WHILE LOOP challenges05.py
@@ -0,0 +1,22 @@
+compnum = 50
+guess = int(input("Enter a number: "))
+count = 1
+anotherGuess = 'Y'
+
+while guess != compnum:
+    while anotherGuess == 'Y':
+        if guess < compnum:
+            print("Your guess is too low")
+            anotherGuess = str(input("Do you want another guess? (Y/N): ")).upper()
+            guess = int(input("Enter another number: "))
+            count = count + 1
+        elif guess > compnum:
+            print("Your guess is too high")
+            anotherGuess = str(input("Do you want another guess? (Y/N): ")).upper()
+            guess = int(input("Enter another number: "))
+            count = count + 1
+        else:
+            anotherGuess = 'N'
+
+print("")
+print("Well done! You guessed the number in",count,"attempts.")
