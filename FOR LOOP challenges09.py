1dbe615533e2ed4c23c9455a6c11285e72f9f878
ClayPalmerOPHS <71497229+ClayPalmerOPHS@users.noreply.github.com>
2021-02-08 15:01:00 +0000

    Create FOR LOOP challenges09.py

---
+++ FOR LOOP challenges09.py
@@ -0,0 +1,12 @@
+invites = int(input("How many people do you want to invite to your party? "))
+
+if invites < 10:
+    for i in range(invites):
+        print("")
+        name = str(input("Enter the name of someone you want to invite: "))
+        print("")
+        print(name,"has been invited")
+    print("")
+    print("We have invited as many people as you wanted")
+else:
+    print("Too many people.")
