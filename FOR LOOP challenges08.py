296990775e44d660d5f42fdc921448bfbc6b6305
ClayPalmerOPHS <71497229+ClayPalmerOPHS@users.noreply.github.com>
2021-02-08 14:41:40 +0000

    Create FOR LOOP challenges08.py

---
+++ FOR LOOP challenges08.py
@@ -0,0 +1,12 @@
+count= str(input("Which way do you want to count? Enter 'u' for up, or enter 'd' for down: "))
+
+if count == 'u':
+    topNum = int(input("What number do you want to count up to: "))
+    for i in range(1,topNum+1,+1):
+        print(i)
+elif count == 'd':
+    downNum = int(input("Enter a number below 20: "))
+    for k in range(20,downNum-1,-1):
+        print(k)
+else:
+    print("I dont understand")
