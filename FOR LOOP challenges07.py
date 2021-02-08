b45bba34226b205d8fafb6e822262992e8d86ce8
ClayPalmerOPHS <71497229+ClayPalmerOPHS@users.noreply.github.com>
2021-02-08 14:33:32 +0000

    Create FOR LOOP challenges07.py

---
+++ FOR LOOP challenges07.py
@@ -0,0 +1,14 @@
+total = 0
+
+for i in range(0,5):
+    print("")
+    num = int(input("Enter a number: "))
+    print("")
+    add = str(input("Do you want to add that number to the total,(Y/N): ")).upper()
+    if add == 'Y':
+        total = total+num
+    else:
+        total = total
+
+print("")
+print("The total of the numbers you entered is",total)
