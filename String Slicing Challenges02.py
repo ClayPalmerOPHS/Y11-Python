e297619c2b251d595a137230bf57ecb6054e0e47
ClayPalmerOPHS <71497229+ClayPalmerOPHS@users.noreply.github.com>
2021-02-08 15:12:04 +0000

    Create String Slicing Challenges02.py

---
+++ String Slicing Challenges02.py
@@ -0,0 +1,9 @@
+forename = str(input("Enter your first name: "))
+surname = str(input("Enter your second name: "))
+
+fullName = forename + surname
+
+length = len(fullName)
+
+print(fullName)
+print("The length of your name is",length)
