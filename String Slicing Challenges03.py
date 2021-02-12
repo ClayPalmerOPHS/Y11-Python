ec8a1230e471cbe4d38ce40e1d91225acc5ba758
ClayPalmerOPHS <71497229+ClayPalmerOPHS@users.noreply.github.com>
2021-02-12 10:12:06 +0000

    Create String Slicing Challenges03.py

---
+++ String Slicing Challenges03.py
@@ -0,0 +1,6 @@
+forename = str(input("Enter your forename in lower case: ")).lower()
+surname = str(input("Enter your surname in lower case: ")).lower()
+space = " "
+
+fullname = forename + space + surname
+print(fullname)
