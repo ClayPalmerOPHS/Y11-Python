ce540deb3150241a9e6083228ae79cbb0b3c9190
ClayPalmerOPHS <71497229+ClayPalmerOPHS@users.noreply.github.com>
2021-02-12 10:23:08 +0000

    Create String Slicing Challenges04.py

---
+++ String Slicing Challenges04.py
@@ -0,0 +1,9 @@
+rhyme = str(input("Enter the first line of a nursery rhyme: "))
+length = len(rhyme)
+print("This has",length,"letters in it")
+
+startNum = int(input("Enter a starting number: "))
+endNum = int(input("Enter an ending number: "))
+
+section = rhyme[startNum:endNum]
+print(section)
