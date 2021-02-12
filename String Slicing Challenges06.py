4f4edf0ca433415bb794bea9ee084cb9f663f435
ClayPalmerOPHS <71497229+ClayPalmerOPHS@users.noreply.github.com>
2021-02-12 10:31:16 +0000

    Create String Slicing Challenges06.py

---
+++ String Slicing Challenges06.py
@@ -0,0 +1,14 @@
+firstName = str(input("Enter your first name: "))
+firstNameU = (firstName).upper()
+firstNameL = (firstName).lower()
+
+
+length = len(firstName)
+
+if length < 5:
+    lastName = str(input("Enter your last name: "))
+    fullname = firstName + lastName
+    firstName
+    print(firstNameU)
+else:
+    print(firstNameL)
